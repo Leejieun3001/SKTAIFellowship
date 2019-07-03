7948518dc8981ffeeac27ebf2d5d6471690693fe
jieun <jieun3001@gmail.com>
2019-07-03 22:57:44 +0900

    init project (add the mapbox dependency)

---
+++ src/practice/mapboxPractice/jieun/SimpleMap/app/src/main/java/aifellowship/skt/or/kr/simplemap/MainActivity.java
@@ -0,0 +1,13 @@
+package aifellowship.skt.or.kr.simplemap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
