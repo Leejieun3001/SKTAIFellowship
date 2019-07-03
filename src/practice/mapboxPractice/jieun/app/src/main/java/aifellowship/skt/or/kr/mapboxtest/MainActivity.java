20d467675348b4e60e67dc6127b09af37e4d3517
jieun <jieun3001@gmail.com>
2019-07-03 17:43:17 +0900

    set access toekn and user permission

--- src/practice/mapboxPractice/jieun/app/src/main/java/aifellowship/skt/or/kr/mapboxtest/MainActivity.java
+++ src/practice/mapboxPractice/jieun/app/src/main/java/aifellowship/skt/or/kr/mapboxtest/MainActivity.java
@@ -3,11 +3,21 @@ package aifellowship.skt.or.kr.mapboxtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    // Mapbox Access token
+        Mapbox.getInstance(getApplicationContext(), getString(R.string.mapbox_access_token));
+
+
+
     }
 }
