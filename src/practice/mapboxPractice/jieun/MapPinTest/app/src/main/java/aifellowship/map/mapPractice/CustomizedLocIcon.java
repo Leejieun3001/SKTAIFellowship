56e83c53cbde1b4ed7ef4844bc2743838c3f632a
jieun <jieun3001@gmail.com>
2019-07-07 00:36:18 +0900

    MapPinTest init project

---
+++ src/practice/mapboxPractice/jieun/MapPinTest/app/src/main/java/aifellowship/map/mapPractice/CustomizedLocIcon.java
@@ -0,0 +1,13 @@
+package aifellowship.map.mapPractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomizedLocIcon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customized_loc_icon);
+    }
+}
--- src/practice/mapboxPractice/jieun/SimpleMap/app/src/main/java/aifellowship/skt/or/kr/simplemap/MainActivity.java
+++ src/practice/mapboxPractice/jieun/SimpleMap/app/src/main/java/aifellowship/skt/or/kr/simplemap/MainActivity.java
@@ -13,44 +13,70 @@ import com.mapbox.mapboxsdk.maps.Style;
 public class MainActivity extends AppCompatActivity {
     private MapView mapView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
-
-
         setContentView(R.layout.activity_main);
-
-
         mapView = (MapView) findViewById(R.id.mapView);
-
         mapView.onCreate(savedInstanceState);
 
         mapView.getMapAsync(new OnMapReadyCallback() {
-
             @Override
-
             public void onMapReady(@NonNull MapboxMap mapboxMap) {
-
-
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-
                     @Override
-
                     public void onStyleLoaded(@NonNull Style style) {
-
-
                         // Map is set up and the style has loaded. Now you can add data or make other map adjustments
-
-
                     }
-
                 });
 
             }
 
         });
+    }
+
+    // Add the mapView lifecycle to the activity's lifecycle methods
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
 
