a5d99fb9dbbf2aa0aa4666d5d88d0979d980b685
jieun <jieun3001@gmail.com>
2019-07-08 23:29:10 +0900

    Mapjieun1.1 - add current location, custom icon ( 단말기 현재 위치에 지정 이미지 아이콘 띄워주기)

---
+++ src/practice/mapboxPractice/jieun/app/src/main/java/com/skt/flashbase/gis/Currentlocation/CustomCurrentLoc.java
@@ -0,0 +1,207 @@
+package com.skt.flashbase.gis.Currentlocation;
+
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.LocationComponentOptions;
+import com.mapbox.mapboxsdk.location.OnCameraTrackingChangedListener;
+import com.mapbox.mapboxsdk.location.OnLocationClickListener;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.skt.flashbase.gis.R;
+
+import java.util.List;
+
+public class CustomCurrentLoc extends AppCompatActivity implements OnMapReadyCallback, OnLocationClickListener, PermissionsListener, OnCameraTrackingChangedListener {
+
+    private PermissionsManager permissionsManager;
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private LocationComponent locationComponent;
+    private boolean isInTrackingMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // get mapbox instance
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+        setContentView(R.layout.activity_custom_current_loc);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+
+    // Called when the map is ready to be used
+    // 지도가 사용될 준비가 되었을때 콜백
+    //이 인터페이스의 인스턴스가 객체 MapFragment또는 MapView객체 에 설정 되면 onMapReady()메소드는, 맵의 사용 준비가 끝났을 때에 트리거되어 null의 인스턴스를 제공
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.LIGHT, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                enableLocationComponent(style);
+            }
+        });
+    }
+
+    @SuppressWarnings({"MossingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        //check if permissions are enabled and if nor request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            // Create and customize the LocationComponent's options
+            LocationComponentOptions customLocationComponentOptions = LocationComponentOptions.builder(this)
+                    .elevation(5)
+                    .accuracyAlpha(.6f)
+                    .accuracyColor(Color.RED)
+                    .foregroundDrawable(R.drawable.here)
+                    .build();
+            //Get and instance of the component
+            locationComponent = mapboxMap.getLocationComponent();
+
+            LocationComponentActivationOptions locationComponentActivationOptions =
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                            .locationComponentOptions(customLocationComponentOptions)
+                            .build();
+
+            //Activation with options
+            locationComponent.activateLocationComponent(locationComponentActivationOptions);
+
+            //Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            //set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+            //set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+
+            //Add the location icon click listener
+            locationComponent.addOnLocationClickListener(this);
+
+            findViewById(R.id.back_to_camera_tracking_mode).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (!isInTrackingMode) {
+                        isInTrackingMode = true;
+                        locationComponent.setCameraMode(CameraMode.TRACKING);
+                        locationComponent.zoomWhileTracking(16f);
+                        //  Toast.makeText(LocationComponentOptionsActivity.this, getString(R.string.tracking_enabled),
+                        //        Toast.LENGTH_SHORT).show();
+                    } else {
+                        //  Toast.makeText(LocationComponentOptionsActivity.this, getString(R.string.tracking_already_enabled),
+                        //         Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    @Override
+    public void onLocationComponentClick() {
+        if (locationComponent.getLastKnownLocation() != null) {
+            Toast.makeText(this, String.format(getString(R.string.user_location_permission_explanation),
+                    locationComponent.getLastKnownLocation().getLatitude(),
+                    locationComponent.getLastKnownLocation().getLongitude()), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onCameraTrackingDismissed() {
+        isInTrackingMode = false;
+    }
+
+    @Override
+    public void onCameraTrackingChanged(int currentMode) {
+// Empty on purpose
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
--- src/practice/mapboxPractice/jieun/app/src/main/java/com/skt/flashbase/gis/MainActivity.java
+++ src/practice/mapboxPractice/jieun/app/src/main/java/com/skt/flashbase/gis/MainActivity.java
@@ -1,13 +1,38 @@
 package com.skt.flashbase.gis;
 
+import android.content.Intent;
+import android.support.design.shape.CutCornerTreatment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.skt.flashbase.gis.Currentlocation.CustomCurrentLoc;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        onClickEvent();
     }
+
+    void onClickEvent() {
+        Button mainGoCustomCurrentBtn = (Button) findViewById(R.id.main_goCustomCurrent_btn);
+        mainGoCustomCurrentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CustomCurrentLoc.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
 }
