ee90bad464e92d49107864b97b622baa074e802c
이솔 <sol6252@naver.com>
2019-07-10 16:44:46 +0900

    190710
    과제끝

---
+++ src/practice/mapboxPractice/sol/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.AnimatedIconMovement.AnimatedIconMovement;
+import com.example.myapplication.BasicMap.BasicMap;
+import com.example.myapplication.CircleLayerClusters.CircleLayerClusters;
+import com.example.myapplication.CreateHotspots.CreateHotspots;
+import com.example.myapplication.DataTimeLapse.DataTimeLapse;
+import com.example.myapplication.DeviceLocation.LocationCamera;
+import com.example.myapplication.VariableLabelPlacement.VariableLabelPlacementActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button basicMapBtn = (Button) findViewById(R.id.basic_map_btn) ;
+        Button IconMoveBtn = (Button) findViewById(R.id.Icon_move_btn) ;
+        Button VaryLabelBtn = (Button) findViewById(R.id.variable_label_btn) ;
+        Button CircleLayerBtn = (Button) findViewById(R.id.circle_layer_btn) ;
+        Button CreateHotspotsBtn = (Button) findViewById(R.id.create_hotspots_btn) ;
+        Button DataTimeLapseBtn = (Button) findViewById(R.id.data_time_lapse_btn) ;
+        Button LocationCamBtn = (Button) findViewById(R.id.location_cam_btn) ;
+        basicMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, BasicMap.class);
+                startActivity(intent);
+            }
+        });
+        VaryLabelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, VariableLabelPlacementActivity.class);
+                startActivity(intent);
+            }
+        });
+        IconMoveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, AnimatedIconMovement.class);
+                startActivity(intent);
+            }
+        });
+        CircleLayerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, CircleLayerClusters.class);
+                startActivity(intent);
+            }
+        });
+        CreateHotspotsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, CreateHotspots.class);
+                startActivity(intent);
+            }
+        });
+        DataTimeLapseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, DataTimeLapse.class);
+                startActivity(intent);
+            }
+        });
+        LocationCamBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, LocationCamera.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
