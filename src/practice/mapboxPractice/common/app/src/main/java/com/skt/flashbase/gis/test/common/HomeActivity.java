cce54830bfd72b6911c4db33109a10690c16b7ee
jieun <jieun3001@gmail.com>
2019-07-25 12:04:44 +0900

    map mark 아이콘 변경, floating btn 이벤트 추가 ( Layer visibility 변경 가능)

--- src/practice/mapboxPractice/common/app/src/main/java/com/skt/flashbase/gis/test/common/HomeActivity.java
+++ src/practice/mapboxPractice/common/app/src/main/java/com/skt/flashbase/gis/test/common/HomeActivity.java
@@ -7,11 +7,15 @@ import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.ImageDecoder;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
@@ -21,10 +25,14 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.style.layers.CircleLayer;
+import com.mapbox.mapboxsdk.style.layers.Layer;
 import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.style.sources.ImageSource;
+import com.mapbox.mapboxsdk.style.sources.Source;
+import com.mapbox.mapboxsdk.utils.BitmapUtils;
 import com.opencsv.CSVReader;
 import com.skt.flashbase.gis.test.R;
 import com.skt.flashbase.gis.test.roomDB.Place;
@@ -36,25 +44,19 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleColor;
+import static com.mapbox.mapboxsdk.style.layers.Property.VISIBLE;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textAllowOverlap;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textColor;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textField;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textIgnorePlacement;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textOffset;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textSize;
-import static java.nio.file.Paths.get;
+
 
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private static final String SOURCE_ID = "SOURCE_ID";
-    private static final String ICON_ID = "ICON_ID";
-    private static final String LAYER_ID = "LAYER_ID";
+    private static final String SOURCE_ID_Foodtruck = "Foodtruck";
+    private static final String ICON_ID_Foodtruck = "Foodtruck";
+    private static final String LAYER_ID_Foodtruck = "Foodtruck";
+    private static final String SOURCE_ID_Tour = "Tour";
+    private static final String ICON_ID_Tour = "Tour";
+    private static final String LAYER_ID_Tour = "Tour";
     private MapView mapView;
     SQLiteDatabase db;
     private PlaceViewModel mPlaceViewModel;
@@ -65,8 +67,6 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SharedPreferences pref = getSharedPreferences("isFirst", Activity.MODE_PRIVATE);
-        boolean isFirst = pref.getBoolean("isFirst", true);
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_home);
         mapView = findViewById(R.id.mapView);
@@ -74,17 +74,6 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.getMapAsync(this);
         //Model Provider 생성
         mPlaceViewModel = ViewModelProviders.of(this).get(PlaceViewModel.class);
-
-        //SharedPreferences 처음 실행하는 경우에만 sqLite에 저장
-        if (isFirst) {
-            CSVtoSqLite();
-            SharedPreferences.Editor editor = pref.edit();
-            editor.putBoolean("isFirst", false);
-            editor.commit();
-        }
-
-        //observe : model의 LiveData를 관찰
-
 
     }
 
@@ -104,35 +93,6 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
 //        }
 //        results.close();
 
-        List<Feature> tourPlaceList = new ArrayList<>();
-
-        for (int i = 0; i < pinPlaceTour.size(); i++) {
-            Double longitude = pinPlaceTour.get(i).getPLongitude();
-            Double latitude = pinPlaceTour.get(i).getPLatitude();
-            tourPlaceList.add(Feature.fromGeometry(
-                    Point.fromLngLat(longitude, latitude)));
-
-        }
-
-
-        //맵스타일 지정
-        mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/mapbox/cjf4m44iw0uza2spb3q0a7s41")
-                .withImage(ICON_ID, BitmapFactory.decodeResource(
-                        HomeActivity.this.getResources(), R.drawable.pin))
-                .withSource(new GeoJsonSource(SOURCE_ID,
-                        FeatureCollection.fromFeatures(tourPlaceList)))
-
-                .withLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
-                        .withProperties(iconImage(ICON_ID),
-                                iconAllowOverlap(true),
-                                iconOffset(new Float[]{0f, -9f}))
-                ), new Style.OnStyleLoaded() {
-            @Override
-            public void onStyleLoaded(@NonNull Style style) {
-
-
-            }
-        });
 
         List<Feature> FoodTruckPlaceList = new ArrayList<>();
 
@@ -143,34 +103,88 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
                     Point.fromLngLat(longitude, latitude)));
 
         }
+        List<Feature> tourPlaceList = new ArrayList<>();
 
-        //맵스타일 지정
-        mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/mapbox/cjf4m44iw0uza2spb3q0a7s41")
-                .withImage(ICON_ID, BitmapFactory.decodeResource(
-                        HomeActivity.this.getResources(), R.drawable.mapbox_marker_icon_default))
-                .withSource(new GeoJsonSource(SOURCE_ID,
-                        FeatureCollection.fromFeatures(FoodTruckPlaceList)))
-                  .withLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
-                        .withProperties(iconImage(ICON_ID),
-                                iconAllowOverlap(true),
-                                iconOffset(new Float[]{0f, -9f}))
-                ), new Style.OnStyleLoaded() {
+        for (int i = 0; i < pinPlaceTour.size(); i++) {
+            Double longitude = pinPlaceTour.get(i).getPLongitude();
+            Double latitude = pinPlaceTour.get(i).getPLatitude();
+            tourPlaceList.add(Feature.fromGeometry(
+                    Point.fromLngLat(longitude, latitude)));
+        }
+
+        mapboxMap.setStyle(Style.OUTDOORS, new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
+                style.addImageAsync(ICON_ID_Foodtruck, BitmapUtils.getBitmapFromDrawable(
+                        getResources().getDrawable(R.drawable.pin_foodtruck_tmp)));
+                Source FoodTruck = new GeoJsonSource(SOURCE_ID_Foodtruck,
+                        FeatureCollection.fromFeatures(FoodTruckPlaceList));
+                style.addSource(FoodTruck);
+                SymbolLayer FoodTruckLayer = new SymbolLayer(LAYER_ID_Foodtruck, SOURCE_ID_Foodtruck)
+                        .withProperties(PropertyFactory.iconImage(ICON_ID_Foodtruck), PropertyFactory.visibility(VISIBLE), iconAllowOverlap(true), iconOffset(new Float[]{0f, -9f}));
+                style.addLayer(FoodTruckLayer);
+
+                style.addImageAsync(ICON_ID_Tour, BitmapUtils.getBitmapFromDrawable(
+                        getResources().getDrawable(R.drawable.pin_tour_tmp)));
+                Source Tour = new GeoJsonSource(SOURCE_ID_Tour,
+                        FeatureCollection.fromFeatures(tourPlaceList));
+                style.addSource(Tour);
+                SymbolLayer TourLayer = new SymbolLayer(LAYER_ID_Tour, SOURCE_ID_Tour)
+                        .withProperties(PropertyFactory.iconImage(ICON_ID_Tour), PropertyFactory.visibility(VISIBLE), iconAllowOverlap(true), iconOffset(new Float[]{0f, -9f}));
+                style.addLayer(TourLayer);
 
 
+                FloatingActionButton homeTourFab = findViewById(R.id.home_landmark_fab);
+                homeTourFab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setLayerVisible(LAYER_ID_Tour, style);
+                    }
+                });
+
+
+                FloatingActionButton homeFoodtruckFab = findViewById(R.id.home_food_fab);
+                homeFoodtruckFab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setLayerVisible(LAYER_ID_Foodtruck, style);
+                    }
+                });
             }
         });
+    }
 
-
+    private void setLayerVisible(String layerId, @NonNull Style loadedMapStyle) {
+        Layer layer = loadedMapStyle.getLayer(layerId);
+        if (layer == null) {
+            return;
+        }
+        if (VISIBLE.equals(layer.getVisibility().getValue())) {
+            layer.setProperties(
+                    PropertyFactory.visibility(Property.NONE)
+            );
+        } else {
+            layer.setProperties(
+                    PropertyFactory.visibility(VISIBLE)
+            );
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
         mapView.onResume();
+        SharedPreferences pref = getSharedPreferences("isFirst", Activity.MODE_PRIVATE);
+        boolean isFirst = pref.getBoolean("isFirst", true);
+        //SharedPreferences 처음 실행하는 경우에만 sqLite에 저장
+        if (isFirst) {
+            CSVtoSqLite();
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putBoolean("isFirst", false);
+            editor.commit();
+        }
 
-        //roomdb방식
+        //roomdb 방식, 관광지 데이터 조회
         mPlaceViewModel.getAllTourPlace().observe(this, new Observer<List<Place>>() {
             @Override
             public void onChanged(@Nullable List<Place> places) {
@@ -179,6 +193,7 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         });
+        //roomdb 방식, 푸드트럭 데이터 조회
         mPlaceViewModel.getAllFoodtruckPlace().observe(this, new Observer<List<Place>>() {
             @Override
             public void onChanged(@Nullable List<Place> places) {
@@ -187,8 +202,6 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         });
-
-
     }
 
     @Override
@@ -230,19 +243,22 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     void CSVtoSqLite() {
         final DBHelper dbHelper = new DBHelper(getApplicationContext(), "location.db", null, 1);
         try {
+            // 카테고리 1, 관광지
             CSVReader read = new CSVReader(new InputStreamReader(getResources().openRawResource(R.raw.korea_landmark_standard_data), "EUC-KR"));
             String[] record = null;
             //CSV 파일을 읽으면서 동시에 SqLite에 저장
             while ((record = read.readNext()) != null) {
                 // Log.i("CSV 파일 읽기", "이름: " + record[0] + ", 위도: " + record[4] + ", 경도: " + record[5]);
                 if (!record[4].equals("위도")) {
+                    //saHelper 이용
                     dbHelper.insertLandmark(record[0], Double.parseDouble(record[4]), Double.parseDouble(record[5]));
-                    //room db 이용 /카테고리 1 =관광지
+                    //room db 이용, 카테고리 1 =관광지
                     Place place = new Place(0, 1, record[0], Double.parseDouble(record[4]), Double.parseDouble(record[5]));
                     mPlaceViewModel.insert(place);
                 }
             }
 
+            //카테고리 2, 푸드트럭
             read = new CSVReader(new InputStreamReader(getResources().openRawResource(R.raw.foodtruck_permission_area), "EUC-KR"));
             record = null;
             //CSV 파일을 읽으면서 동시에 SqLite에 저장
@@ -256,13 +272,10 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
                         mPlaceViewModel.insert(place);
                     }
                 }
-
-
             }
         } catch (IOException ex) {
             // handle exception
         } finally {
-
         }
     }
 }
