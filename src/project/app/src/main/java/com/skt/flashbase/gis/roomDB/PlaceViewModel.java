9cd94b26bfbd0052395b30a9baf73c64f5914a74
Leejieun3001 <jieun3001@gmail.com>
2019-08-19 23:52:30 +0900

    add room db

---
+++ src/project/app/src/main/java/com/skt/flashbase/gis/roomDB/PlaceViewModel.java
@@ -0,0 +1,51 @@
+package com.skt.flashbase.gis.roomDB;
+
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.skt.flashbase.gis.roomDB.dataModel.PlaceRepository;
+
+import java.util.List;
+
+public class PlaceViewModel extends AndroidViewModel {
+
+    private PlaceRepository mPlaceRepository;
+    private LiveData<List<Place>> mAllPlace;
+
+    private LiveData<List<Place>> mAllTourPlace;
+    private LiveData<List<Place>> mAllFoodtruckPlace;
+    private LiveData<Place> mPlaceInfo;
+
+    public PlaceViewModel(Application application) {
+        super(application);
+        mPlaceRepository = new PlaceRepository(application);
+        mAllPlace = mPlaceRepository.getAllPlace();
+        mAllTourPlace = mPlaceRepository.getAllTour();
+        mAllFoodtruckPlace = mPlaceRepository.getAllFoodtruck();
+
+    }
+
+    public LiveData<List<Place>> getAllPlace() {
+        return mAllPlace;
+    }
+
+    public LiveData<List<Place>> getAllTourPlace() {
+        return mAllTourPlace;
+    }
+
+    public LiveData<List<Place>> getAllFoodtruckPlace() {
+        return mAllFoodtruckPlace;
+    }
+
+    public LiveData<Place> getPlaceInfo(int idx) {
+        mPlaceInfo = mPlaceRepository.getPlaceInfo(idx);
+        return mPlaceInfo;
+    }
+
+    public void insert(Place place) {
+        mPlaceRepository.insert(place);
+    }
+}
