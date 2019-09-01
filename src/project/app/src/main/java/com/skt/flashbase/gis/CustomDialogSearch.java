89771ca1813c658361f8785758ef92eaad7e37b8
Leejieun3001 <jieun3001@gmail.com>
2019-09-01 13:43:17 +0900

    add  choose sex and age SharedPreferences

--- src/project/app/src/main/java/com/skt/flashbase/gis/CustomDialogSearch.java
+++ src/project/app/src/main/java/com/skt/flashbase/gis/CustomDialogSearch.java
@@ -1,5 +1,6 @@
 package com.skt.flashbase.gis;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -31,6 +32,7 @@ public class CustomDialogSearch extends Dialog implements View.OnClickListener {
     private CheckBox searchWoman60CheckBox;
     private TextView searchchoseWholeBtn;
     private TextView searchcancelWholeBtn;
+    private SharedPreferences pref = getContext().getSharedPreferences("ChoseSex_Age", Activity.MODE_PRIVATE);
 
     private Context context;
 
@@ -82,6 +84,32 @@ public class CustomDialogSearch extends Dialog implements View.OnClickListener {
 
         searchchoseWholeBtn = (TextView) findViewById(R.id.search_choseWhole_textView);
         searchcancelWholeBtn = (TextView) findViewById(R.id.search_cancelWhole_textView);
+        boolean Man10 = pref.getBoolean("Man10", false);
+        searchMan10CheckBox.setChecked(Man10);
+        boolean Man20 = pref.getBoolean("Man20", false);
+        searchMan20CheckBox.setChecked(Man20);
+        boolean Man30 = pref.getBoolean("Man30", false);
+        searchMan30CheckBox.setChecked(Man30);
+        boolean Man40 = pref.getBoolean("Man40", false);
+        searchMan40CheckBox.setChecked(Man40);
+        boolean Man50 = pref.getBoolean("Man50", false);
+        searchMan50CheckBox.setChecked(Man50);
+        boolean Man60 = pref.getBoolean("Man60", false);
+        searchMan60CheckBox.setChecked(Man60);
+
+        boolean Woman10 = pref.getBoolean("Woman10", false);
+        searchWoman10CheckBox.setChecked(Woman10);
+        boolean Woman20 = pref.getBoolean("Woman20", false);
+        searchWoman20CheckBox.setChecked(Woman20);
+        boolean Woman30 = pref.getBoolean("Woman30", false);
+        searchWoman30CheckBox.setChecked(Woman30);
+        boolean Woman40 = pref.getBoolean("Woman40", false);
+        searchWoman40CheckBox.setChecked(Woman40);
+        boolean Woman50 = pref.getBoolean("Woman50", false);
+        searchWoman50CheckBox.setChecked(Woman50);
+        boolean Woman60 = pref.getBoolean("Woman60", false);
+        searchWoman60CheckBox.setChecked(Woman60);
+
 
         //add button Listener
         searchOKBtn.setOnClickListener(this);
@@ -94,21 +122,82 @@ public class CustomDialogSearch extends Dialog implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.search_ok_btn:
-                //각각 check box 값 저장
                 String result = "";
+                SharedPreferences.Editor editor = pref.edit();
 
-                if (searchWoman10CheckBox.isChecked()) result += "10대 여성 ";
-                if (searchWoman20CheckBox.isChecked()) result += "20대 여성 ";
-                if (searchWoman30CheckBox.isChecked()) result += "30대 여성 ";
-                if (searchWoman40CheckBox.isChecked()) result += "40대 여성 ";
-                if (searchWoman50CheckBox.isChecked()) result += "50대 여성 ";
-                if (searchWoman60CheckBox.isChecked()) result += "60대 여성 ";
-                if (searchMan10CheckBox.isChecked()) result += "10대 남성 ";
-                if (searchMan20CheckBox.isChecked()) result += "20대 남성 ";
-                if (searchMan30CheckBox.isChecked()) result += "30대 남성 ";
-                if (searchMan40CheckBox.isChecked()) result += "40대 남성 ";
-                if (searchMan50CheckBox.isChecked()) result += "50대 남성 ";
-                if (searchMan60CheckBox.isChecked()) result += "60대 남성 ";
+                if (searchWoman10CheckBox.isChecked()) {
+                    result += "10대 여성 ";
+                    editor.putBoolean("Woman10", true);
+                } else {
+                    editor.putBoolean("Woman10", false);
+                }
+                if (searchWoman20CheckBox.isChecked()) {
+                    result += "20대 여성 ";
+                    editor.putBoolean("Woman20", true);
+                } else {
+                    editor.putBoolean("Woman20", false);
+                }
+                if (searchWoman30CheckBox.isChecked()) {
+                    result += "30대 여성 ";
+                    editor.putBoolean("Woman30", true);
+                } else {
+                    editor.putBoolean("Woman30", false);
+                }
+                if (searchWoman40CheckBox.isChecked()) {
+                    result += "40대 여성 ";
+                    editor.putBoolean("Woman40", true);
+                } else {
+                    editor.putBoolean("Woman40", false);
+                }
+                if (searchWoman50CheckBox.isChecked()) {
+                    result += "50대 여성 ";
+                    editor.putBoolean("Woman50", true);
+                } else {
+                    editor.putBoolean("Woman50", false);
+                }
+                if (searchWoman60CheckBox.isChecked()) {
+                    result += "60대 여성 ";
+                    editor.putBoolean("Woman60", true);
+                } else {
+                    editor.putBoolean("Woman60", false);
+                }
+                if (searchMan10CheckBox.isChecked()) {
+                    result += "10대 남성 ";
+                    editor.putBoolean("Man10", true);
+                } else {
+                    editor.putBoolean("Man10", false);
+                }
+                if (searchMan20CheckBox.isChecked()) {
+                    result += "20대 남성 ";
+                    editor.putBoolean("Man20", true);
+                } else {
+                    editor.putBoolean("Man20", false);
+                }
+                if (searchMan30CheckBox.isChecked()) {
+                    result += "30대 남성 ";
+                    editor.putBoolean("Man30", true);
+                } else {
+                    editor.putBoolean("Man30", false);
+                }
+                if (searchMan40CheckBox.isChecked()) {
+                    result += "40대 남성 ";
+                    editor.putBoolean("Man40", true);
+                } else {
+                    editor.putBoolean("Man40", false);
+                }
+                if (searchMan50CheckBox.isChecked()) {
+                    result += "50대 남성 ";
+                    editor.putBoolean("Man50", true);
+                } else {
+                    editor.putBoolean("Man50", false);
+                }
+                if (searchMan60CheckBox.isChecked()) {
+                    result += "60대 남성 ";
+                    editor.putBoolean("Man60", true);
+                } else {
+                    editor.putBoolean("Man60", false);
+                }
+                editor.commit();
 
                 customDialogSearchListener.onPositiveClicked(result);
                 dismiss();
@@ -130,6 +219,7 @@ public class CustomDialogSearch extends Dialog implements View.OnClickListener {
                 searchMan40CheckBox.setChecked(true);
                 searchMan50CheckBox.setChecked(true);
                 searchMan60CheckBox.setChecked(true);
+                storeData();
                 break;
             case R.id.search_cancelWhole_textView:
                 searchWoman10CheckBox.setChecked(false);
@@ -144,9 +234,95 @@ public class CustomDialogSearch extends Dialog implements View.OnClickListener {
                 searchMan40CheckBox.setChecked(false);
                 searchMan50CheckBox.setChecked(false);
                 searchMan60CheckBox.setChecked(false);
+                storeData();
                 break;
 
         }
+    }
+
+    public void storeData() {
+        SharedPreferences.Editor editor = pref.edit();
+        String result = "";
+        if (searchWoman10CheckBox.isChecked()) {
+            result += "10대 여성 ";
+            editor.putBoolean("Woman10", true);
+        } else {
+            editor.putBoolean("Woman10", false);
+        }
+        if (searchWoman20CheckBox.isChecked()) {
+            result += "20대 여성 ";
+            editor.putBoolean("Woman20", true);
+        } else {
+            editor.putBoolean("Woman20", false);
+        }
+        if (searchWoman30CheckBox.isChecked()) {
+            result += "30대 여성 ";
+            editor.putBoolean("Woman30", true);
+        } else {
+            editor.putBoolean("Woman30", false);
+        }
+        if (searchWoman40CheckBox.isChecked()) {
+            result += "40대 여성 ";
+            editor.putBoolean("Woman40", true);
+        } else {
+            editor.putBoolean("Woman40", false);
+        }
+
+        if (searchWoman50CheckBox.isChecked()) {
+            result += "50대 여성 ";
+            editor.putBoolean("Woman50", true);
+        } else {
+            editor.putBoolean("Woman50", false);
+        }
+        if (searchWoman60CheckBox.isChecked()) {
+            result += "60대 여성 ";
+            editor.putBoolean("Woman60", true);
+        } else {
+            editor.putBoolean("Woman60", false);
+        }
+        if (searchMan10CheckBox.isChecked()) {
+            result += "10대 남성 ";
+            editor.putBoolean("Man10", true);
+        } else {
+            editor.putBoolean("Man10", false);
+        }
+        if (searchMan20CheckBox.isChecked()) {
+            result += "20대 남성 ";
+            editor.putBoolean("Man20", true);
+        } else {
+            editor.putBoolean("Man20", false);
+
+        }
+        if (searchMan30CheckBox.isChecked()) {
+            result += "30대 남성 ";
+            editor.putBoolean("Man30", true);
+        } else {
+            editor.putBoolean("Man30", false);
+
+        }
+        if (searchMan40CheckBox.isChecked()) {
+            result += "40대 남성 ";
+            editor.putBoolean("Man40", true);
+        } else {
+            editor.putBoolean("Man40", false);
+
+        }
+        if (searchMan50CheckBox.isChecked()) {
+            result += "50대 남성 ";
+            editor.putBoolean("Man50", true);
+        } else {
+            editor.putBoolean("Man50", false);
+
+        }
+        if (searchMan60CheckBox.isChecked()) {
+            result += "60대 남성 ";
+            editor.putBoolean("Man60", true);
+        } else {
+            editor.putBoolean("Man60", false);
+
+        }
+        editor.commit();
+
     }
 }
 
